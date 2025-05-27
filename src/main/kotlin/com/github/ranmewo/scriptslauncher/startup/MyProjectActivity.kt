0a8a1308ec33d1afd35a420d1ef09103c0ac134d
GitHub Action <action@github.com>
2025-05-27 00:41:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/ranmewo/scriptslauncher/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/ranmewo/scriptslauncher/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.ranmewo.scriptslauncher.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
